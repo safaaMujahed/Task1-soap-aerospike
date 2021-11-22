1338ea1e9fa08625d8d908d37829904b025c8dd4
safaaMujahede <safaa.mujahed@exalt.email>
2021-11-22 10:09:44 +0200

    finish the soap api web service on countires example without testing the soap ws

---
+++ src/main/java/com/example/producingwebservice/configuration/WebServiceConfig.java
@@ -0,0 +1,56 @@
+//Create a new class named WebServiceConfiguration.java which will contain Spring Web services related beans configurations
+package com.example.producingwebservice.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "countries")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CountriesPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://demo.com");
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+    }
+}
+
+//    In Spring, the objects that form the backbone of your application and that are managed by the Spring IoC container
+//        are called beans. A bean is an object that is instantiated, assembled, and otherwise managed by a Spring IoC container.
+//explain why we need to do this : https://www.baeldung.com/spring-bean
+//Spring WS uses a different servlet type for handling SOAP messages: MessageDispatcherServlet. It is important to inject and set ApplicationContext
+// to MessageDispatcherServlet. Without that, Spring WS will not automatically detect Spring beans.
+//
+//        Naming this bean messageDispatcherServlet does not replace Spring Boot’s default DispatcherServlet bean.
+//
+//        DefaultMethodEndpointAdapter configures the annotation-driven Spring WS programming model. This makes it possible to use the various annotations, such as @Endpoint (mentioned earlier).
+//
+//        DefaultWsdl11Definition exposes a standard WSDL 1.1 by using XsdSchema
+
+//    You need to specify bean names for MessageDispatcherServlet and DefaultWsdl11Definition. Bean names determine the URL under which the web service and the generated WSDL file are available.
+//        In this case, the WSDL will be available under http://<host>:<port>/ws/countries.wsdl.
---
+++ src/main/java/com/example/producingwebservice/endpoints/CountryEndpoint.java
@@ -0,0 +1,38 @@
+//To create Book service endpoint, we just need to annotate a POJO with Spring WS annotation to take care of SOAP request.
+package com.example.producingwebservice.endpoints;
+
+import com.demo.GetCountryRequest;
+import com.demo.GetCountryResponse;
+import com.example.producingwebservice.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+//    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+    private static final String NAMESPACE_URI = "http://demo.com";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
+//
+//@Endpoint: This annotation is used to register the class with Spring WS for processing incoming SOAP request.
+//@PayloadRoot: This annotation helps Spring WS to pick handler method based on message’s namespace and localPart.
+//@ResponsePayload: This annotation indicates that incoming message will be mapped to method’s request parameter.
+//@ResponsePayload: This annotation is used to Spring WS map the return value to the response payload.
